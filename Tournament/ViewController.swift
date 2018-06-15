b0e7c854e1cf94970a7452f4e3732f99ea86f72a
TJ Sartain <troy.sartain@penske.com>
2018-06-15 16:14:10 -0400

    Initial Commit

---
+++ Tournament/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tournament
+//
+//  Created by TJ Sartain on 6/15/18.
+//  Copyright © 2018 iTrinity, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
